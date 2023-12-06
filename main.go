9f85b6399dc35cabb40352c118c60fd885bbe60b
Haschwalth <pekztor4gung@gmail.com>
2023-12-06 16:42:33 +0800

    fix indent

--- main.go
+++ main.go
@@ -4,20 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type Recipe struct {
-	Name          string    `json:"name"`
-	Tags          []string  `json:"tags"`
-	Ingredients   []string  `json:"ingredients"`
-	Instructions  []string  `json:"instructions"`
-	PublishesdAtt time.Time `json:"publishedAt"`
-}
-
 func main() {
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello World!",
-		})
-	})
-  func main() {
+	r.Run()
 }
